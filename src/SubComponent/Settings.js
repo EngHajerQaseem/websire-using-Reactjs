d4ea88ce8b53aa962de01a5fdb48d074cc03f1be
Hajer Alkhadmi <enggorrey1234@gmail.com>
2021-12-16 11:29:10 +0300

    commit7

--- src/SubComponent/Settings.js
+++ src/SubComponent/Settings.js
@@ -10,18 +10,67 @@ const Settigs = () => {
     document.title = "Settings Page";
   }, []);
 
-  const user = { ID: "3", Name: "Hajer Qaseem", password: "user123" };
-  const languages = [
-    { ID: "1", Lang: "English" },
-    { ID: "2", Lang: "Arabic" }
-  ];
-
   return (
     <React.Fragment>
       <Header />
       <Nav />
       <div className="content">
-      
+        <div className="hnav">
+          <span className="span1">
+            <a href="#div1">General Setting</a>
+          </span>
+          <span className="span2">
+            <a href="#div2">Update Admin Information</a>
+          </span>
+          <span className="span3">
+            <a href="#div3">Change Language</a>
+          </span>
+        </div>
+
+        <div id="div1">
+          <section className="s1">
+            <label>Website Name</label>
+            <input type="text" />
+            <section className="s2">
+              <label>Website Logo</label>
+              <input type="file" />
+            </section>
+            <section className="s3">
+              <label>Change Color</label>
+              <input type="color"/>
+            </section>
+            <button className="save2">Save</button>
+          </section>
+        </div>
+
+        <div id="div2">
+        <section className="s1">
+            <label>User Name</label>
+            <input type="text" />
+            <section className="s2">
+              <label>Admin Profile</label>
+              <input type="file" />
+            </section>
+            <section className="s3">
+              <label>Change Password</label>
+              <input type="password"/>
+            </section>
+            <button className="save3">Save</button>
+          </section>
+        </div>
+
+        <div id="div3">
+          <div className="s4">
+            <label className="l1">Choose Language:</label>
+          <input type="radio" id="html" name="fav_language" value="English" />
+<label for="html">English</label>
+<input type="radio" id="css" name="fav_language" value="Arabic" />
+<label for="css">Arabic</label>
+          </div>
+     
+
+          
+        </div>
       </div>
 
       <Footer />
